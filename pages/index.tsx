bb5a8137b74f24e03dbbb069c8f88a24b83195db
EstebanPerez99 <epm1520@gmail.com>
2022-12-26 17:01:35 -0600

    layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 export default function Home() {
 	return (
-		<div className='flex flex-col'>
-			<div>1</div>
-			<div>2</div>
+		<div className='h-screen w-screen flex flex-col justify-center items-center'>
+			<div className='text-2xl text-center'>
+				Cuál pokemón sería más whitexican?
+			</div>
+			<div className='flex border rounded p-8 justify-between max-w-2xl items-center'>
+				<div className='w-16 h-16 bg-red-200' />
+				<div className='p-8'>vs</div>
+				<div className='w-16 h-16 bg-red-200' />
+			</div>
 		</div>
 	);
 }
